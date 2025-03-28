7188b400e6bd5c97278f1955ae84002f0556b02f
anijumech <54428222+anijumech@users.noreply.github.com>
2025-03-28 20:58:12 +0800

    Update installApp.sh

--- installApp.sh
+++ installApp.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+output=`sudo docker ps|awk '{print $1}'|tail -1`
+if [ -z "$output" ]; then
+    echo "App is not running"
+else
+    echo "Killing the existing App .."
+    sudo docker kill ${output}
+fi
+echo "Starting the App .."
 sudo docker run -p 8080:8080 -d anbanerj11/nodejsapp
